93e7df7676e8145b4dbfff68d7d0f03198694093
echarso <charalampos.souris@gmail.com>
2020-11-05 22:33:49 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -15,5 +15,6 @@ class Controller:
 
     def predict(self):
         print "do staff inside "
+        ## oups that is a steal!!!
         series = read_csv('sample.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
         return tm.predict(series)
