4225d5fdbcfe9de22b76f45c437164cd97d02d88
echaros <charalampos.souris@CharalampossAir.lan>
2021-03-16 17:09:57 +0100

    close file

--- controller.py
+++ controller.py
@@ -13,9 +13,9 @@ class Controller:
         self.name = conf[0]
         self.version = conf[1]
         self.source = conf[2]
+        f.close()
         print(self.name, ' ',self.version, ' ',self.source )
         self.pyfunc_model = pyfunc.load_model('model')
-        f.close()
 
     def predict(self,data):
         print ("do staff inside .......")
