3cd64a2c8a3602a93c3e1079711c8e512a452baf
echaros <charalampos.souris@CharalampossAir.lan>
2021-03-17 21:49:22 +0100

    conf txt file and version validation

--- controller.py
+++ controller.py
@@ -2,18 +2,20 @@ from pandas import read_csv
 from mlflow import pyfunc
 import numpy as np
 import pandas as pd
+import time
+
 
 
 class Controller:
     def __init__(self):
         print( "Create controller  with configuration file !!!!!")
         f = open("conf.txt", "r")
-        #conf = f.read()
-        #conf = conf.split(',\n')
-        self.name = 'foo'
-        self.version = 'version'
-        self.source = 'not found'
-        #f.close()
+        conf = f.read()
+        conf = conf.split(',\n')
+        self.name = conf[0]
+        self.version = conf[1]
+        self.source = conf[2]
+        f.close()
         print(self.name, ' ',self.version, ' ',self.source )
         self.pyfunc_model = pyfunc.load_model('model')
 
@@ -21,16 +23,18 @@ class Controller:
         print ("do staff inside .......")
         print (data)
         print (type(data))
-        print( "****************************9999")
+
         input = pd.DataFrame(columns=data['columns'],data= np.array(data['data']))
-        print(input)
         ret={}
         ret['predictions'] =  self.pyfunc_model.predict(input)
         ret['name']=self.name
         ret['version']=self.version
         ret['source']=self.source
-        print( "----------predict done -----------------")
+        ret['predictioId'] = time.time()
+        ret['input_data']=data['data']
+        ret['columns'] = data['columns']
 
+        print( "----------predict done -----------------")
         print(ret)
         print( "----------predict done -----------------")
 
